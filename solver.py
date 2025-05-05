1c99854f2daa21bb53460221c9cf8aa6c2c73735
Preet Sojitra <psoji001@ucr.edu>
2025-05-05 16:29:53 -0700

    Implement PuzzleNode class with g, h, and f()

--- solver.py
+++ solver.py
@@ -7,3 +7,14 @@ import time
 import sys
 
 GOAL_STATE = (1,2,3,4,5,6,7,8,0)
+
+class PuzzleNode:
+    def __init__(self, state, parent=None, move=None, g=0):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.g = g
+        self.h = 0
+
+    def f(self):
+        return self.g + self.h
